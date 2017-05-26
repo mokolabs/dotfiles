5a840e0c2c3b852a39555d1baf4748b408fea3a4
Patrick Crowley <patrick@mokolabs.com>
2017-05-26 11:35:02 -0700

    Update dotfiles installer to only update files when changes are detected from a git pull

--- install.sh
+++ install.sh
@@ -29,77 +29,90 @@ link() {
   fi
 }
 
-# Get latest changes
-echo "\nUPDATING DOTFILES"
-echo "... Loading SSH key"
-echo "... Switching to ~/.dotfiles directory"
-echo "... Pulling changes from master repo"
-ssh-agent bash -c "ssh-add -A /Users/patrick/.ssh/id_rsa; cd /Users/patrick/.dotfiles; git pull -q origin master;" > /dev/null 2>&1
+# Check for changes
+echo "\nCHECKING FOR CHANGES"
+OUTPUT="$(ssh-agent bash -c 'ssh-add -A /Users/patrick/.ssh/id_rsa /dev/null 2>&1; cd /Users/patrick/.dotfiles; git status -u no | grep "up-to-date";')";
 
-# Bash
-echo "\nBASH PROFILE AND COMPLETION"
-link $dotfiles/bash_profile $HOME/.bash_profile
-link $dotfiles/bash_completion $HOME/.bash_completion
+# If changes found, update dotfiles
+if [ -z "$OUTPUT" ] ; then
 
-# Git
-echo "\nGIT CONFIG AND COMPLETION"
-link $dotfiles/gitconfig $HOME/.gitconfig
-link $dotfiles/gitignore $HOME/.gitignore
-link $dotfiles/git-completion.bash $HOME/.git-completion.bash
-
-# Ruby
-echo "\nRUBY CONFIG"
-link $dotfiles/gemrc $HOME/.gemrc
-
-# Atom
-# (manually copy stylesheet because atom can't use symlinks)
-echo "\nATOM STYLES"
-remove $HOME/.atom/styles.less
-copy $dotfiles/styles.less $HOME/.atom/styles.less
-
-# Launcher
-echo "\nLAUNCHER APPLESCRIPT"
-link $dotfiles/launcher.applescript $HOME/.launcher.applescript
-
-# Services
-echo "\nSERVICES"
-echo "... removed Resize Finder Windows"
-echo "... copied Resize Finder Windows"
-rm -rfd ~/Library/Services/Resize\ Finder\ Windows.workflow/
-cp -R Resize\ Finder\ Windows.workflow/ ~/Library/Services/Resize\ Finder\ Windows.workflow/
-
-# Drive tools
-echo "\nDRIVE TOOLS"
-link $dotfiles/drive_mount.sh $HOME/.drive_mount.sh
-link $dotfiles/drive_unmount.sh $HOME/.drive_unmount.sh
-
-# Dropbox
-echo "\nDROPBOX"
-link $dotfiles/dropbox.remover.sh $HOME/.dropbox.remover.sh
-
-# Crontab
-if [ $# -eq 0 ]; then
+  echo "\nUPDATING DOTFILES"
+  echo "... Loading SSH key"
+  echo "... Switching to ~/.dotfiles directory"
+  echo "... Pulling changes from master repo"
+  (ssh-agent bash -c "ssh-add -A /Users/patrick/.ssh/id_rsa; cd /Users/patrick/.dotfiles; git pull -q origin master;";
   
-  tput sc                             # Save cursor position
-  echo "\nShow sample crontab? (y/n)" # Ask question
-  read -s input                       # Get input for question
-  tput rc                             # Restore cursor to saved position
-  tput el                             # Clear to end of line
-  tput cud1                           # Move cursor down one line
-  tput dl1                            # Delete one cursor line
-  tput dl1                            # Delete one cursor line
-
-  # Show sample crontab if requested
-  if [ "$input" == "y" ] || [ "$input" == "yes" ]; then
-    echo "SAMPLE CRONTAB\n"
-    cat ~/.dotfiles/crontab
+  # Bash
+  echo "\nBASH PROFILE AND COMPLETION"
+  link $dotfiles/bash_profile $HOME/.bash_profile
+  link $dotfiles/bash_completion $HOME/.bash_completion
+  
+  # Git
+  echo "\nGIT CONFIG AND COMPLETION"
+  link $dotfiles/gitconfig $HOME/.gitconfig
+  link $dotfiles/gitignore $HOME/.gitignore
+  link $dotfiles/git-completion.bash $HOME/.git-completion.bash
+  
+  # Ruby
+  echo "\nRUBY CONFIG"
+  link $dotfiles/gemrc $HOME/.gemrc
+  
+  # Atom
+  # (manually copy stylesheet because atom can't use symlinks)
+  echo "\nATOM STYLES"
+  remove $HOME/.atom/styles.less
+  copy $dotfiles/styles.less $HOME/.atom/styles.less
+  
+  # Launcher
+  echo "\nLAUNCHER APPLESCRIPT"
+  link $dotfiles/launcher.applescript $HOME/.launcher.applescript
+  
+  # Services
+  echo "\nSERVICES"
+  echo "... removed Resize Finder Windows"
+  echo "... copied Resize Finder Windows"
+  rm -rfd ~/Library/Services/Resize\ Finder\ Windows.workflow/
+  cp -R Resize\ Finder\ Windows.workflow/ ~/Library/Services/Resize\ Finder\ Windows.workflow/
+  
+  # Drive tools
+  echo "\nDRIVE TOOLS"
+  link $dotfiles/drive_mount.sh $HOME/.drive_mount.sh
+  link $dotfiles/drive_unmount.sh $HOME/.drive_unmount.sh
+  
+  # Dropbox
+  echo "\nDROPBOX"
+  link $dotfiles/dropbox.remover.sh $HOME/.dropbox.remover.sh
+  
+  # Crontab
+  if [ $# -eq 0 ]; then
+    
+    tput sc                             # Save cursor position
+    echo "\nShow sample crontab? (y/n)" # Ask question
+    read -s input                       # Get input for question
+    tput rc                             # Restore cursor to saved position
+    tput el                             # Clear to end of line
+    tput cud1                           # Move cursor down one line
+    tput dl1                            # Delete one cursor line
+    tput dl1                            # Delete one cursor line
+  
+    # Show sample crontab if requested
+    if [ "$input" == "y" ] || [ "$input" == "yes" ]; then
+      echo "SAMPLE CRONTAB\n"
+      cat ~/.dotfiles/crontab
+    fi
+  
+  # Hide question prompt if any command line argument is present
+  # so we can this script via cron and ignore the question
+  # example: `/usr/bin/env sh install.sh bypass`  
+  else
+    
+    echo ""
+  
   fi
-
-# Hide question prompt if any command line argument is present
-# so we can this script via cron and ignore the question
-# example: `/usr/bin/env sh install.sh bypass`  
-else
   
-  echo ""
+# If no changes found, leave dotfiles as-is
+else
+
+  echo "No changes found!";
 
 fi
