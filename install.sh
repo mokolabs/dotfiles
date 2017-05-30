dfa680eaea41ff1edd8b4ce69bb5cb720d23c1ad
Patrick Crowley <patrick@mokolabs.com>
2017-05-30 11:36:51 -0700

    Change voice command to test dotfiles sync propagation

--- install.sh
+++ install.sh
@@ -136,7 +136,7 @@ if [ "$UPDATE" = true ]; then
   timestamp=$(date "$format");  # get current time
   timestamp=${timestamp/AM/am}; # lowercase AM
   timestamp=${timestamp/PM/pm}; # lowercase PM
-  /usr/local/bin/terminal-notifier -sound default -title 'Dotfiles Updated' -message "Your dotfiles were updated at $timestamp."
+  terminal-notifier -sound default -title 'Dotfiles Updated' -message "Your dotfiles were updated at $timestamp."
   
 # If no changes found, leave dotfiles as-is
 else
@@ -144,3 +144,5 @@ else
   echo "\nNo changes found!\n";
   
 fi
+
+say "goodbye";
