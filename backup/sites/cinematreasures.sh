2a89bed71d77e8b8b1fbf8656acae63ea9855195
Patrick Crowley <patrick@mokolabs.com>
2017-06-03 11:14:37 -0700

    Migrate site backup scripts from ruby to bash (since we really don't need ruby at all for these scripts)

---
+++ backup/sites/cinematreasures.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PATH="/usr/local/heroku/bin:/usr/local:/usr/local/bin:usr/local/sbin:/usr/local/share/npm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:$PATH"
+
+# Grab temporary backup link
+/usr/local/heroku/bin/heroku pg:backups public-url --app beekman > /Users/patrick/Sites/beekman/database.url
+
+# Backup database
+TIME=/Users/patrick/Library/Backup/Cinema/$(date +'%Y-%m-%d').sql && curl -o $TIME `cat /Users/patrick/Sites/beekman/database.url`
+
+# Remove temporary backup link
+rm -rfd /Users/patrick/Sites/beekman/database.url
+
+# Notify user
+format="+%l:%M %p" && timestamp=$(date "$format") && timestamp=${timestamp/AM/am} && timestamp=${timestamp/PM/pm} && terminal-notifier -sound default -title "Cinema Treasures" -message "The database was backed up at $timestamp." -group "Cinema Treasures"
---
+++ backup/sites/graffletopia.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PATH="/usr/local/heroku/bin:/usr/local:/usr/local/bin:usr/local/sbin:/usr/local/share/npm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:$PATH"
+
+# Grab temporary backup link
+/usr/local/heroku/bin/heroku pg:backups public-url --app graffletopia > /Users/patrick/Sites/graffletopia/database.url
+
+# Backup database
+TIME=/Users/patrick/Library/Backup/Graffletopia/$(date +'%Y-%m-%d').sql && curl -o $TIME `cat /Users/patrick/Sites/graffletopia/database.url`
+
+# Remove temporary backup link
+rm -rfd /Users/patrick/Sites/graffletopia/database.url
+
+# Notify user
+format="+%l:%M %p" && timestamp=$(date "$format") && timestamp=${timestamp/AM/am} && timestamp=${timestamp/PM/pm} && terminal-notifier -sound default -title "Graffletopia" -message "The database was backed up at $timestamp." -group "Graffletopia"
